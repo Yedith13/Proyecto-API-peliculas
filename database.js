09ac95c83a38782692b8417776a61e88829a6beb
Yedith13 <my.espinoza24@info.uas.edu.mx>
2026-05-28 19:28:00 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,13 +1,15 @@
 import { Sequelize } from 'sequelize';
-// process.env.DATABASE_URL será inyectado dinámicamente por la infraestructura de Render
-const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgresql://pelicula_dd8e_user:ni6HG3t2V7kCPGouCc9KXgwqkbmcLMs8@dpg-d8brd1jtqb8s73a63gh0-a.oregon-postgres.render.com/pelicula_dd8e ', {
-dialect: 'postgres',
-logging: false,
-dialectOptions: {
-ssl: {
-require: true,
-rejectUnauthorized: false // Permite la conexión con los certificados dinámicos de Render
-}
-}
+import 'dotenv/config'; // Activa la lectura del archivo .env
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  }
 });
+
 export default sequelize;
